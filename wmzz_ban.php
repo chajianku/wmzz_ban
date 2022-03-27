29ff0f9ecafe2ebae0fb81de70eb601776ba925a
Weltolk <40228052+Weltolk@users.noreply.github.com>
2022-03-27 14:15:21 +0800

    Update wmzz_ban.php

--- wmzz_ban.php
+++ wmzz_ban.php
@@ -9,20 +9,27 @@ Author Email: kenvix@vip.qq.com
 Author URL: http://zhizhe8.net
 For: V3.1+
 */
-if (!defined('SYSTEM_ROOT')) { die('Insufficient Permissions'); } 
-
-function wmzz_ban_addaction_navi() {
-	?>
-	<li <?php if(isset($_GET['plugin']) && $_GET['plugin'] == 'wmzz_ban') { echo 'class="active"'; } ?>><a href="index.php?plugin=wmzz_ban"><span class="glyphicon glyphicon-ban-circle"></span> 循环封禁</a></li>
-	<?php
+if (!defined('SYSTEM_ROOT')) {
+    die('Insufficient Permissions');
 }
 
-function wmzz_ban_setting_navi() {
-	?>
-	<li><a href="index.php?mod=admin:setplug&plug=wmzz_ban"><span class="glyphicon glyphicon-ban-circle"></span> 循环封禁管理</a></li>
-	<?php
+function wmzz_ban_addaction_navi()
+{
+    ?>
+    <li <?php if (isset($_GET['plugin']) && $_GET['plugin'] == 'wmzz_ban') {
+        echo 'class="active"';
+    } ?>><a href="index.php?plugin=wmzz_ban"><span class="glyphicon glyphicon-ban-circle"></span> 循环封禁</a></li>
+    <?php
 }
 
-addAction('navi_1','wmzz_ban_addaction_navi');
-addAction('navi_7','wmzz_ban_addaction_navi');
-addAction('navi_3','wmzz_ban_setting_navi');
+function wmzz_ban_setting_navi()
+{
+    ?>
+    <li><a href="index.php?mod=admin:setplug&plug=wmzz_ban"><span class="glyphicon glyphicon-ban-circle"></span> 循环封禁管理</a>
+    </li>
+    <?php
+}
+
+addAction('navi_1', 'wmzz_ban_addaction_navi');
+addAction('navi_7', 'wmzz_ban_addaction_navi');
+addAction('navi_3', 'wmzz_ban_setting_navi');

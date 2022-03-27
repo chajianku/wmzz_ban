7204cd5774fceffd974a4bcd4d0a9e9fb045957b
Weltolk <40228052+Weltolk@users.noreply.github.com>
2022-03-27 14:16:47 +0800

    Update wmzz_ban_show.php

--- wmzz_ban_show.php
+++ wmzz_ban_show.php
@@ -1,66 +1,51 @@
-<?php if (!defined('SYSTEM_ROOT')) { die('Insufficient Permissions'); }  
-global $i,$m;
+<?php if (!defined('SYSTEM_ROOT')) {
+    die('Insufficient Permissions');
+}
+global $i, $m;
 $s = unserialize(option::get('plugin_wmzz_ban'));
 if (SYSTEM_PAGE == 'add') {
-	$pid   = !empty($_POST['pid']) ? intval($_POST['pid']) : msg('请选择PID');
-	if (!isset($i['user']['bduss'][$pid])) {
-		msg('PID不存在');
-	}
-	$tieba = !empty($_POST['tieba']) ? addslashes(strip_tags($_POST['tieba'])) : msg('请输入贴吧');
-	if (isset($_POST['date'])) {
-		if (empty($_POST['date'])) {
-			$date = '0';
-		} else {
-			$date = strtotime($_POST['date']);
-		}
-	} else {
-		msg('请输入截止日期');
-	}
-	$value = addslashes(strip_tags($_POST['user']));
-	$tpid = $_POST['tpid'];
-	$m->query("INSERT INTO `".DB_PREFIX."wmzz_ban` (`uid`, `pid`, `tieba`, `user`, `tpid` , `date`) VALUES ('".UID."', '{$pid}', '{$tieba}', '{$value}', '{$tpid}', '{$date}')");
-	ReDirect(SYSTEM_URL . 'index.php?plugin=wmzz_ban&ok');
+    $pid = !empty($_POST['pid']) ? intval($_POST['pid']) : msg('请选择PID');
+    if (!isset($i['user']['bduss'][$pid])) {
+        msg('PID不存在');
+    }
+    $tieba = !empty($_POST['tieba']) ? addslashes(strip_tags($_POST['tieba'])) : msg('请输入贴吧');
+    $portrait = !empty($_POST['portrait']) ? addslashes(strip_tags($_POST['portrait'])) : msg('请输入被封禁人portrait');
+    $portrait = explode("\n", trim($portrait));
+    if (isset($_POST['date'])) {
+        if (empty($_POST['date'])) {
+            $date = '0';
+        } else {
+            $date = strtotime($_POST['date']);
+        }
+    } else {
+        msg('请输入截止日期');
+    }
+    $prefix = "id=tb.";
+    $suffix1 = "&";
+    $suffix2 = "?";
+    foreach ($portrait as $portrait_i) {
+        $portrait_i = trim($portrait_i);
+        if (!empty($portrait_i)) {
+            if (strpos($portrait_i, $prefix)) {
+                $portrait_i = "tb." . explode($prefix, $portrait_i)[1];
+            }
+            if (strpos($portrait_i, $suffix1)) {
+                $portrait_i = explode($suffix1, $portrait_i)[0];
+            }
+            if (strpos($portrait_i, $suffix2)) {
+                $portrait_i = explode($suffix2, $portrait_i)[0];
+            }
+//            if(strlen($portrait_i)=="")
+            $m->query("INSERT INTO `" . DB_PREFIX . "wmzz_ban` (`uid`, `pid`, `tieba`, `portrait` , `date`) VALUES ('" . UID . "', '{$pid}', '{$tieba}', '{$portrait_i}', '{$date}')");
+        }
+    }
+    ReDirect(SYSTEM_URL . 'index.php?plugin=wmzz_ban&ok');
 } elseif (SYSTEM_PAGE == 'del') {
-	$id = isset($_GET['id']) ? intval($_GET['id']) : msg('缺少ID');
-	$m->query("DELETE FROM `".DB_PREFIX."wmzz_ban` WHERE `uid` = ".UID." AND `id` = ".$id);
-	ReDirect(SYSTEM_URL . 'index.php?plugin=wmzz_ban&ok');
-} elseif(SYSTEM_PAGE == 'getpid'){
-	$username = $_GET['user'];
-	$tieba = $_GET['tieba'];
-	$pid = $_GET['pid'];
-	$option = array(
-		'word' => mb_convert_encoding($tieba,'gbk','UTF-8'),
-		'op_type' => '',
-		'stype' => 'post_uname',
-		'svalue' =>  mb_convert_encoding($username,'gbk','UTF-8'),
-		'date_type' => 'on',
-		'startTime' => '',
-		'begin' => '',
-		'endTime' => '',
-		'end' => ''
-	);
-	$bduss = misc::getCookie($pid);
-	$query = http_build_query($option);
-	$c = new wcurl('http://tieba.baidu.com/bawu2/platform/listPostLog?'. $query);
-	$c->addcookie('BDUSS='.$bduss);
-	$html = $c->get();
-	$html = mb_convert_encoding($html,'UTF-8','gbk');
-	$preg ="/<article class=\"post_wrapper clearfix\"><div class=\"post_meta\"><div class=\"post_author\">.*?<h1><a target=\"_blank\" href=\".*?pid=(.*?)\".*?<\/article>/i";
-	preg_match_all($preg, $html, $info);
-	$pid = [];
-	foreach ($info[1] as $value) {
-		$pidarray = explode('#',$value);
-		$pid[] = (float)$pidarray[1];
-		$pid[] = (float)$pidarray[0];
-	}
-	$maxpid =  max($pid);
-	if($maxpid > 100000000000){
-		echo json_encode(array('status' => 'ok','pid'=>$maxpid));
-		return;
-	}
-	echo json_encode(array('status' => 'error'));
-}else {
-	loadhead();
-	require SYSTEM_ROOT.'/plugins/wmzz_ban/show.php';
-	loadfoot(); 
+    $id = isset($_GET['id']) ? intval($_GET['id']) : msg('缺少ID');
+    $m->query("DELETE FROM `" . DB_PREFIX . "wmzz_ban` WHERE `uid` = " . UID . " AND `id` = " . $id);
+    ReDirect(SYSTEM_URL . 'index.php?plugin=wmzz_ban&ok');
+} else {
+    loadhead();
+    require SYSTEM_ROOT . '/plugins/wmzz_ban/show.php';
+    loadfoot();
 } 

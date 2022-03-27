0ff2df3e9dabadce443ec1832bb1b670599f83bf
Weltolk <40228052+Weltolk@users.noreply.github.com>
2022-03-27 14:16:12 +0800

    Update wmzz_ban_cron.php

--- wmzz_ban_cron.php
+++ wmzz_ban_cron.php
@@ -1,45 +1,50 @@
-<?php if (!defined('SYSTEM_ROOT')) { die('Insufficient Permissions'); }
+<?php if (!defined('SYSTEM_ROOT')) {
+    die('Insufficient Permissions');
+}
 
 /**
  * 获取封禁类型
  * @param $date 封禁截止日期
  */
-function wmzz_ban_getTime($date) {
-	return '1';
+function wmzz_ban_getTime($date)
+{
+    return '1';
 }
 
-function cron_wmzz_ban() {
-	global $m;
-	$s = unserialize(option::get('plugin_wmzz_ban'));
-	$now   = strtotime(date('Y-m-d'));
-	$y = $m->query("SELECT * FROM `".DB_PREFIX."wmzz_ban` WHERE `nextdo` <= '{$now}' LIMIT {$s['limit']}");
-	while ($x = $m->fetch_array($y)) {
-		$r = wmzz_ban_getTime($x['date']);
-		if ($r != '-1') {
-			$bduss = misc::getCookie($x['pid']);
-			$c = new wcurl('http://tieba.baidu.com/pmc/blockid');
-			$c->addcookie('BDUSS='.$bduss);
-			$option = array(
-				'user_name[]' => $x['user'],
-				'day' => $r,
-				'fid' => misc::getFid($x['tieba']),
-				'tbs' => misc::getTbs($x['uid'] , $bduss),
-				'ie' => 'utf-8',
-				'reason' => $s['msg']
-			);
-			if(!empty($x['tpid'])){
-				$option['pid[]']  = $x['tpid'];
-			}
-			$res = $c->post($option);
-			$res = json_decode($res,TRUE);
-			if($res['errno'] == 0){
-				$next = $now + ( $r * 86400 );
-				$m->query("UPDATE `".DB_PREFIX."wmzz_ban` SET `nextdo` = '{$next}' WHERE `id` = '{$x['id']}'");
-			} else if( $res['errno'] == 74 ){    //用户名不存在   224011 需要验证码
-				$m->query("DELETE FROM `".DB_PREFIX."wmzz_ban` WHERE `id` = '{$x['id']}'");
-			}
-		} else {
-			$m->query("DELETE FROM `".DB_PREFIX."wmzz_ban` WHERE `id` = '{$x['id']}'");
-		}
-	}
+function cron_wmzz_ban()
+{
+    global $m;
+    $s = unserialize(option::get('plugin_wmzz_ban'));
+    $now = strtotime(date('Y-m-d'));
+    $y = $m->query("SELECT * FROM `" . DB_PREFIX . "wmzz_ban` WHERE `nextdo` <= '{$now}' LIMIT {$s['limit']}");
+    while ($x = $m->fetch_array($y)) {
+        $r = wmzz_ban_getTime($x['date']);
+        if ($r != '-1') {
+            $bduss = misc::getCookie($x['pid']);
+            $c = new wcurl('http://tieba.baidu.com/pmc/blockid');
+            $c->addcookie('BDUSS=' . $bduss);
+            $option = array(
+                'day' => $r,
+                'fid' => misc::getFid($x['tieba']),
+                'tbs' => misc::getTbs($x['uid'], $bduss),
+//                'ie' => 'utf-8',
+                'ie' => 'gbk',
+                'user_name[]' => '',
+                'nick_name[]' => '',
+                'portrait[]' => $x['portrait'],
+                'pid[]' => '',
+                'reason' => $s['msg']
+            );
+            $res = $c->post($option);
+            $res = json_decode($res, TRUE);
+            if ($res['errno'] == 0) {
+                $next = $now + ($r * 86400);
+                $m->query("UPDATE `" . DB_PREFIX . "wmzz_ban` SET `nextdo` = '{$next}' WHERE `id` = '{$x['id']}'");
+            } else if ($res['errno'] == 74) {    //用户名不存在   224011 需要验证码
+                $m->query("DELETE FROM `" . DB_PREFIX . "wmzz_ban` WHERE `id` = '{$x['id']}'");
+            }
+        } else {
+            $m->query("DELETE FROM `" . DB_PREFIX . "wmzz_ban` WHERE `id` = '{$x['id']}'");
+        }
+    }
 }

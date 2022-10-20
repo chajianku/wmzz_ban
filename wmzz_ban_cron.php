33ac18e7ca112b7341237c01d064f6d7223eb3ad
Weltolk <40228052+Weltolk@users.noreply.github.com>
2022-10-20 17:54:25 +0800

    Update wmzz_ban_cron.php

--- wmzz_ban_cron.php
+++ wmzz_ban_cron.php
@@ -6,13 +6,32 @@
  * 获取封禁类型
  * @param $date 封禁截止日期
  */
-function wmzz_ban_getTime($date)
+function wmzz_ban_getTime($now, $date, $day)
 {
+    $day_list = [
+        '1',
+        '3',
+        '10',
+    ];
+    $diff = $date - $now;
+    $diff_day = ceil($diff / 86400);
     $result = '';
     if ($date == '0') {
-        $result = '0';
+        $result = $day;
+    } else if ($diff < 0) {
+        return '-1';
     } else {
-        $result = $date - strtotime(date('Y-m-d'));
+        if ($diff_day >= $day) {
+            $result = $day;
+        } else if ($diff_day == 0) {
+            $result = '1';
+        } else {
+            foreach (array_reverse($day_list) as $day_i) {
+                if ($diff_day >= $day_i) {
+                    $result = $day_i;
+                }
+            }
+        }
     }
     return $result;
 }
@@ -24,14 +43,13 @@ function cron_wmzz_ban()
     $now = strtotime(date('Y-m-d'));
     $y = $m->query("SELECT * FROM `" . DB_PREFIX . "wmzz_ban` WHERE `nextdo` <= '{$now}' LIMIT {$s['limit']}");
     while ($x = $m->fetch_array($y)) {
-        $r = wmzz_ban_getTime($x['date']);
+        $r = wmzz_ban_getTime($now, $x['date'], $x['day']);
         if ($r >= '0') {
-            $day = '1';
             $bduss = misc::getCookie($x['pid']);
             $c = new wcurl('http://tieba.baidu.com/pmc/blockid');
             $c->addcookie('BDUSS=' . $bduss);
             $option = array(
-                'day' => $day,
+                'day' => $r,
                 'fid' => misc::getFid($x['tieba']),
                 'tbs' => misc::getTbs($x['uid'], $bduss),
 //                'ie' => 'utf-8',
@@ -45,7 +63,7 @@ function cron_wmzz_ban()
             $res = $c->post($option);
             $res = json_decode($res, TRUE);
             if ($res['errno'] == 0) {
-                $next = $now + ($day * 86400);
+                $next = $now + ($r * 86400);
                 $m->query("UPDATE `" . DB_PREFIX . "wmzz_ban` SET `nextdo` = '{$next}' WHERE `id` = '{$x['id']}'");
             }
 //            else if ($res['errno'] == 74) {    //用户名不存在   224011 需要验证码
